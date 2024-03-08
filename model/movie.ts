eeb66adbd586c8d2078d18f30d15eb876492ed98
LilithZ00 <160513219+LilithZ00@users.noreply.github.com>
2024-03-08 21:00:39 +0700

    Add files via upload

---
+++ model/movie.ts
@@ -0,0 +1,8 @@
+export interface movie_req {
+    id_movie: Number;
+    name_movie: String;
+    img_movie: String;
+    trailer_movie: String;
+    story_movie: String;
+    score_movie: Number;
+}
